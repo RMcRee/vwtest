9ec6f69bbe90be7ec64f0d84229f9efe95705e90
Randall McRee <randall.mcree@gmail.com>
2014-11-24 11:51:20 -0800

    Minimized example
    Took out extraneous parameters, subsetted training data to only 30
    examples. Predicted label is now 10. Not surprising since we no longer
    have training data for many labels. But overall behaviour still
    incorrect--first library prediction matches batch, subsequent are
    incorrect/different.

--- libvw_simple_jna.cc
+++ libvw_simple_jna.cc
@@ -5,29 +5,30 @@
 
 using namespace std;
 
-vw* vw[50]; // array of vw instances, caller keeps them all straight by specifying an index
+vw* vw; // array of vw instances, caller keeps them all straight by specifying an index
 
 extern "C" void initialize(int index, const char* command) {
-  vw[index] = VW::initialize(command);
+  vw = VW::initialize(command);
   //cerr << "Java initialize called on " << vw << endl;
 }
 
 extern "C" double getPrediction(int index, const char* example_string) {
   //cerr << "Example string is " << example_string << endl;
-  example *vec2 = VW::read_example(*vw[index], (char*)example_string);
-  vw[index]->learn(vec2);
+  example *vec2 = VW::read_example(*vw, (char*)example_string);
+  vw->learn(vec2);
   float prediction;
-  if (vw[index]->p->lp.parse_label == MULTICLASS::mc_label.parse_label) {
+  if (vw->p->lp.parse_label == MULTICLASS::mc_label.parse_label) {
             prediction = ((MULTICLASS::multiclass*) vec2->ld)->prediction;
   }
   else {
             prediction = ((label_data*) vec2->ld)->prediction;
   }
-  VW::finish_example(*vw[index], vec2);
+
+  VW::finish_example(*vw, vec2);
   //cerr << "example prediction = " << prediction << endl;
   return prediction;
 }
 
 extern "C" void closeInstance(int index) {
-  VW::finish(*vw[index]);
+  VW::finish(*vw);
 }
